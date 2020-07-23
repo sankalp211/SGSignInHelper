61263e85a3e73fd2a732ca6f77048aba12916949
Sankalp Gupta <sankalpgupta021193@gmail.com>
2020-07-23 10:26:35 +0530

    Initial Commit

---
+++ SignInHelper/SignInHelper.h
@@ -0,0 +1,19 @@
+//
+//  SignInHelper.h
+//  SignInHelper
+//
+//  Created by Sankalp Gupta on 23/07/20.
+//  Copyright © 2020 Sankalp Gupta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SignInHelper.
+FOUNDATION_EXPORT double SignInHelperVersionNumber;
+
+//! Project version string for SignInHelper.
+FOUNDATION_EXPORT const unsigned char SignInHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SignInHelper/PublicHeader.h>
+
+
