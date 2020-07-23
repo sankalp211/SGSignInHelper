2b66b6d8dd552ea16543501c4fed883eaac981c3
Sankalp Gupta <sankalpgupta021193@gmail.com>
2020-07-23 10:54:35 +0530

    SignInHelperTests -> AppleSignInManagerTests

---
+++ SignInHelperTests/AppleSignInManagerTests.swift
@@ -0,0 +1,22 @@
+//
+//  AppleSignInManagerTests.swift
+//  SignInHelperTests
+//
+//  Created by Sankalp Gupta on 23/07/20.
+//  Copyright © 2020 Sankalp Gupta. All rights reserved.
+//
+
+import XCTest
+@testable import SignInHelper
+
+class AppleSignInManagerTests: XCTestCase {
+	
+	override func setUpWithError() throws {
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+	}
+	
+	override func tearDownWithError() throws {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+	}
+	
+}
